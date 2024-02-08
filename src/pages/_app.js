df8fd976b00ce9d557bff14dc37d772df2b9998a
Adarsh Das <adarsh.191605@ncit.edu.np>
2024-02-08 08:49:11 +0545

    meta tags add

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -34,8 +34,22 @@ export default function App({ Component, pageProps }) {
       className={`${reenie.variable} ${tanker.variable} ${satoshi.variable} `}
     >
       <Head>
-        <title>Adarsh Das - Developer</title>
+        <title>Adarsh Das</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="shortcut icon" href="favicon.png" />
+        <meta property="og:image" content="meta.png" />
+        <meta
+          name="description"
+          content="Full Stack Developer based on Kathmandu, Nepal. Fluent in ReactJS, NextJS and NodeJS"
+        />
+        <meta name="og:title" content="Adarsh Das | FullStack Developer" />
+        <meta name="og:url" content="adasrshdas.com.np" />
+        <meta
+          name="og:description"
+          content="Full Stack Developer based on Kathmandu, Nepal."
+        />
+        <meta name="author" content="Adarsh Das" />
+        <meta name="twitter:card" content="summary_large" />
       </Head>
       <Component {...pageProps} theme={theme} setTheme={setTheme} />
     </main>
